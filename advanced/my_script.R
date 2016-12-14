e5d19a0c182478dac6eac5d26c24ba0bd8caabb0
jules32 <jstewart@nceas.ucsb.edu>
2016-12-14 09:38:13 -0800

    final updates after runthough in vizlab

--- advanced/my_script.R
+++ advanced/my_script.R
@@ -1,15 +1,20 @@
-## Filter the Gapminder dataset for a specific country
+## Function to calculate GDP in the Gapminder dataset. 
+
+## Imagine this was a huge analysis. 
 
 ## setup
 library(tidyverse) # install.packages('tidyverse')
 library(gapminder) # install.packages('gapminder')
 
-## get a sense of the data
-summary(gapminder)
-
-## filter a country of interest
-data_from_my_script <- gapminder %>%
-  filter(country == 'Germany')
-
+calc_gdp = function(my_country){
+  
+  ## add gdp column with gdp calc
+  d <- gapminder %>%
+    mutate(gdp = pop * gdpPercap) %>%
+    filter(country == my_country)
+  
+  return(d)
+  
+}
 
 
